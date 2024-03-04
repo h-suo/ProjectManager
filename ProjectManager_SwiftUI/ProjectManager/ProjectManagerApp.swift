f0bb85c9ca07a3c5fc95cacb29b804df33dbf06b
h-suo <phstoo1021@naver.com>
2024-03-04 15:47:12 +0900

    🚚build: SwiftUI 리펙토링 파일 생성

---
+++ ProjectManager_SwiftUI/ProjectManager/ProjectManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectManagerApp.swift
+//  ProjectManager
+//
+//  Created by Erick on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
