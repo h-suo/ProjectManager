382eaa42cdb528c80ff782c9bf9b1bbe82bf24e8
h-suo <phstoo1021@naver.com>
2024-03-05 16:04:04 +0900

    🩹Chore: 컨벤션 변경

--- ProjectManager_SwiftUI/ProjectManager/Application/ProjectManagerApp.swift
+++ ProjectManager_SwiftUI/ProjectManager/Application/ProjectManagerApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct ProjectManagerApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
