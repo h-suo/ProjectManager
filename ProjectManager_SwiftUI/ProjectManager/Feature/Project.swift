47383ce12dad0cee2b2d416467472ee3cc9b1c06
h-suo <phstoo1021@naver.com>
2024-03-05 16:04:53 +0900

    ✨Feat: Project 객체 생성

---
+++ ProjectManager_SwiftUI/ProjectManager/Feature/Project.swift
@@ -0,0 +1,36 @@
+//
+//  Project.swift
+//  ProjectManager
+//
+//  Created by Erick on 3/5/24.
+//
+
+import Foundation
+
+enum State: String, CaseIterable {
+  case toDo = "TODO"
+  case doing = "DOING"
+  case done = "DONE"
+}
+
+struct Project: Identifiable {
+  let id: UUID
+  var title: String
+  var body: String
+  var deadline: Date
+  var state: State
+  
+  init(
+    id: UUID = UUID(),
+    title: String,
+    body: String,
+    deadline: Date,
+    state: State
+  ) {
+    self.id = id
+    self.title = title
+    self.body = body
+    self.deadline = deadline
+    self.state = state
+  }
+}
