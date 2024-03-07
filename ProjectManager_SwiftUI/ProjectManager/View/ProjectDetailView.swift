b8667afee96bb55c760febd3d0e0f4a95af3bab0
h-suo <phstoo1021@naver.com>
2024-03-07 14:16:31 +0900

    ✨Feat: ProjectDetailView 생성

---
+++ ProjectManager_SwiftUI/ProjectManager/View/ProjectDetailView.swift
@@ -0,0 +1,68 @@
+//
+//  ProjectDetailView.swift
+//  ProjectManager
+//
+//  Created by Erick on 3/7/24.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+struct ProjectDetailView: View {
+  
+  @Bindable 
+  var store: StoreOf<ProjectDetailFeature>
+  
+  var body: some View {
+    NavigationStack {
+      VStack(alignment: .center) {
+        TextField("Title", text: $store.project.title.sending(\.setTitle))
+          .border(.black, width: 0.5)
+          .font(.title3)
+        
+        DatePicker(
+          "",
+          selection: $store.project.deadline.sending(\.setDeadLine)
+        )
+          .datePickerStyle(.wheel)
+          .frame(width: 0)
+        
+        TextEditor(text: $store.project.body.sending(\.setBody))
+          .border(.black, width: 0.5)
+      }
+      .padding()
+      .navigationTitle("TODO")
+      .navigationBarTitleDisplayMode(.inline)
+      .toolbarBackground(Color(.systemFill), for: .navigationBar)
+      .toolbarBackground(.visible, for: .navigationBar)
+      .toolbar {
+        ToolbarItem(placement: .topBarLeading) {
+          Button("Cancel") {
+            store.send(.cancelButtonTapped)
+          }
+        }
+        
+        ToolbarItem {
+          Button("Save") {
+            store.send(.saveButtonTapped)
+          }
+        }
+      }
+    }
+  }
+}
+
+#Preview {
+  ProjectDetailView(
+    store: Store(
+      initialState: ProjectDetailFeature.State(
+        project: Project(
+          title: "Title",
+          body: "Body",
+          deadline: Date()
+        )
+      ),
+      reducer: { ProjectDetailFeature() }
+    )
+  )
+}
