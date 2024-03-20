39a10ee0f199418e082985945c93fbf042d8e5bf
h-suo <phstoo1021@naver.com>
2024-03-20 15:07:35 +0900

    ✨Feat: Project 관리를 위한 ProjectDatabase 생성 및 의존성 등록

---
+++ ProjectManager_SwiftUI/ProjectManager/Environment/ProjectDataBase.swift
@@ -0,0 +1,61 @@
+//
+//  ProjectDataBase.swift
+//  ProjectManager
+//
+//  Created by Erick on 3/19/24.
+//
+
+import Foundation
+import SwiftData
+import Dependencies
+
+extension DependencyValues {
+  var projectDatabase: ProjectDatabase {
+    get { self[ProjectDatabase.self] }
+    set { self[ProjectDatabase.self] = newValue }
+  }
+}
+
+struct ProjectDatabase {
+  var fetch: () throws -> [Project]
+  var add: (Project) throws -> Void
+  var delete: (Project) throws -> Void
+  
+  enum DatabaseError: Error {
+    case addFailed
+    case deleteFailed
+  }
+}
+
+extension ProjectDatabase: DependencyKey {
+  static var liveValue: ProjectDatabase = Self(
+    fetch: {
+      do {
+        @Dependency(\.database.context) var context
+        let projectContext = try context()
+        let descriptor = FetchDescriptor<Project>(sortBy: [SortDescriptor(\.deadline)])
+        return try projectContext.fetch(descriptor)
+      } catch {
+        return []
+      }
+    },
+    add: { project in
+      do {
+        @Dependency(\.database.context) var context
+        let projectContext = try context()
+        projectContext.insert(project)
+      } catch {
+        throw DatabaseError.addFailed
+      }
+    },
+    delete: { project in
+      do {
+        @Dependency(\.database.context) var context
+        let projectContext = try context()
+        projectContext.delete(project)
+      } catch {
+        throw DatabaseError.deleteFailed
+      }
+    }
+  )
+}
